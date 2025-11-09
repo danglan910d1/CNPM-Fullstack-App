889a1c1305ce4c2afb12a57ef48d4873e6e42561
Dang Lan <danglan910d1@gmail.com>
2025-11-09 16:46:54 +0700

    Configure Git: Stop tracking .env, node_modules, and dist directories

--- backend/dist/models/User.d.ts
+++
@@ -1,9 +0,0 @@
-import { Document, Model } from "mongoose";
-export interface IUser extends Document {
-    name: string;
-    email: string;
-    createdAt?: Date;
-}
-declare const User: Model<IUser>;
-export default User;
-//# sourceMappingURL=User.d.ts.map
--- backend/dist/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-// src/routes/userRoutes.ts
-import { Router } from "express";
-import { getUsers, createUser } from "../controllers/userController.js";
-const router = Router();
-router.get("/", getUsers);
-router.post("/", createUser);
-export default router;
-//# sourceMappingURL=userRoutes.js.map
